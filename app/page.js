310097ec834891e08933d76d79752d615f30369d
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-01-06 11:05:24 +0530

    added boilerplate code ✅

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+  <main>
+    <h1 className='text-3xl text-center font-mono font-semibold bg-gray-200 p-3'>Hello 👋 - next-tailwind-starter</h1>
+  </main> 
+   )
+}
