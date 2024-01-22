a12baf02f97127a8d7ac74d05844fac052a9823c
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-01-23 00:44:50 +0530

    chores: minor fixes ✅

--- components/common/FeatureImg.jsx
+++ components/common/FeatureImg.jsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import React from "react";
 
-const FeatureImg = ({ image }) => {
+const FeatureImg = ({ image, ...props }) => {
   return (
     <>
       <picture className="mb-2">
-        <Image className="w-full rounded-lg" src={image} alt="feature image" />
+        <Image
+          className="w-full rounded-lg"
+          src={image}
+          alt="feature image"
+          {...props}
+        />
       </picture>
     </>
   );
