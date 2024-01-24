f6ff488af856ef9af3a39efd37fa9e92a25ac62c
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-01-24 21:58:58 +0530

    chores: fixing api error ✅

--- app/api/spotify/route.js
+++ app/api/spotify/route.js
@@ -1,23 +1,23 @@
-import { currentlyPlayingSong } from "@/lib/spotify";
-import { NextResponse } from "next/server";
+// import { currentlyPlayingSong } from "@/lib/spotify";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-  const response = await currentlyPlayingSong();
+// export async function GET() {
+//   const response = await currentlyPlayingSong();
 
-  if (!response.status === 204 || !response.status > 400) {
-    return NextResponse.json({ isPlaying: false });
-  }
+//   if (!response.status === 204 || !response.status > 400) {
+//     return NextResponse.json({ isPlaying: false });
+//   }
 
-  const result = await response.json();
+//   const result = await response.json();
 
-  const data = {
-    isPlaying: result?.is_playing,
-    title: result?.item?.name,
-    songUrl: result?.item?.external_urls?.spotify,
-    albumImg: result?.item?.album?.images[0]?.url,
-    artist: result?.item?.artists?.map((artist) => artist?.name).join(","),
-    trackType: result?.currently_playing_type,
-  };
+//   const data = {
+//     isPlaying: result?.is_playing,
+//     title: result?.item?.name,
+//     songUrl: result?.item?.external_urls?.spotify,
+//     albumImg: result?.item?.album?.images[0]?.url,
+//     artist: result?.item?.artists?.map((artist) => artist?.name).join(","),
+//     trackType: result?.currently_playing_type,
+//   };
 
-  return NextResponse.json(data);
-}
+//   return NextResponse.json(data);
+// }
