23564faafee837b7b1bda25950029e719e78343e
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-17 00:08:05 +0530

    chores: fixed smooth scroll ✅

--- components/providers/smooth-scroll.jsx
+++ components/providers/smooth-scroll.jsx
@@ -3,5 +3,9 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 export const SmoothScroll = ({ children }) => {
-  return <ReactLenis root>{children}</ReactLenis>;
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+      {children}
+    </ReactLenis>
+  );
 };
