320184d93f2ecc4a87e1e1a9017cda2fe0eb29af
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-06-19 02:31:47 +0530

    chores: added crafts in nav tab also added resume page

--- app/crafts/page.jsx
+++ app/crafts/page.jsx
@@ -1,7 +1,30 @@
+import Article from "@/components/common/Article";
 import React from "react";
+import {
+  AuthorInfo,
+  Content,
+  ContentContainer,
+  Mood,
+  Title,
+  UserProfile,
+} from "../(home)/page";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <Article className={"dark:hover:bg-[#212529] hover:bg-[#f1f3f5]"}>
+      <UserProfile />
+      <ContentContainer>
+        <AuthorInfo author={"Himanshu"} />
+        <Mood MoodEmoji={"🎨"} MoodText={"Feelin' artsy"} />
+        <Title>Crafts</Title>
+        <Content
+          className={
+            "prose prose-light dark:prose-dark prose-a:decoration-2 prose-img:blog-article-img prose-blockquote:bg-[#f1f3f5] dark:prose-blockquote:bg-[#212529]"
+          }
+        ></Content>
+      </ContentContainer>
+    </Article>
+  );
 };
 
 export default page;
