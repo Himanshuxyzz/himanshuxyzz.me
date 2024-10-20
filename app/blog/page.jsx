33d1fff62a9e394044278a0a094c4d84cae082ba
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-10-20 11:33:58 +0530

    chores: hotfix

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -3,15 +3,44 @@ import {
   AuthorInfo,
   ContentContainer,
   Mood,
+  Title,
   UserProfile,
 } from "@/app/(home)/page";
-import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import { cn, formatDate } from "@/lib/utils";
 import { allBlogs } from "@/.contentlayer/generated";
 import compareDesc from "date-fns/compareDesc";
 import TweetArticle from "@/components/common/TweetArticle";
-import { ArticleItem, ArticleList } from "@/components/blog/ArticleComponents";
 
-const Page = () => {
+export const ArticleItem = ({ text, href, date, className }) => {
+  return (
+    <div
+      className={cn(" hover:bg-[#f1f3f5] dark:hover:bg-[#212529]", className)}
+    >
+      <Link href={href}>
+        <div className=" border-b-2 border-b-link-bg w-full flex justify-between px-1 py-2 md:py-[8px] flex-col md:flex-row">
+          <h2 className="font-semibold text-md">
+            <span>{text}</span>
+          </h2>
+          <div className="text-sm font-medium dark:text-[#868e96] text-[#495057]">
+            {date}
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export const ArticleList = ({ title, children }) => {
+  return (
+    <>
+      <Title>{title}</Title>
+      <div className="space-y-2">{children}</div>
+    </>
+  );
+};
+
+const page = () => {
   const posts = allBlogs.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date))
   );
@@ -39,4 +68,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default page;
